435a9e9d1f4525fa2d908b58fe3044c7059eba5c
andrewsoonqn <andrewsoonqn@gmail.com>
2025-06-12 00:21:02 +0800

    Add test case, fix bug
    Add test case to test `get_data()` and JSON decoding. Fix bug (typo) in `get_data()`

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
 import json
 
+
 def get_data():
-    return '{"tiam": "bibendum", "lacus": 23.5, "sellus": False}'
+    # 'F' in "False" should be lowercase instead
+    # return '{"tiam": "bibendum", "lacus": 23.5, "sellus": False}'
+    return '{"tiam": "bibendum", "lacus": 23.5, "sellus": false}'
+
 
 def main():
     data = json.loads(get_data())
     print(data)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
--- tests.py
+++ tests.py
@@ -1,5 +1,18 @@
 import unittest
+import json
+
+from main import get_data
+
 
 class TestGeneral(unittest.TestCase):
-   def test_placeholder(self):
-       self.assertTrue(True)
+    def test_placeholder(self):
+        self.assertTrue(True)
+
+    def test_json_decoding(self):
+        # Checks whether get_data() returns valid JSON
+        # that matches expected results
+        data = json.loads(get_data())
+        self.assertIsInstance(data, dict)
+        self.assertEqual(data["tiam"], "bibendum")
+        self.assertEqual(data["lacus"], 23.5)
+        self.assertEqual(data["sellus"], False)
